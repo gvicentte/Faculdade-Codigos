4d21db718ee027d3296c2d82b5a654487fbc246a
Gabriel Vicentte <gvicentte10@gmail.com>
2025-07-17 08:58:29 -0300

    Refactor Main method to use async/await for database connection and query execution

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,22 +1,34 @@
 using System;
+using System.Data.Common;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using Npgsql; // Npgsql is the .NET data provider for PostgreSQL
 
 class Program
 {
-    static void Main()
+    static async Task Main()
     {
         string nome;
         Console.Write("Ola Informe seu nome: ");
         nome = Console.ReadLine();
         Console.WriteLine("Ola seu nome e {0}", nome);
         string connectionString = "Host=localhost;Username=postgres;Password=1508;Database=postgres"; //conectou ao banco dessa vez rodando pelo VsCode
-        using(var connection=new NpgsqlConnection(connectionString))
+        using (var connection = new NpgsqlConnection(connectionString))
         {
             connection.Open();
             Console.WriteLine("Conectado ao banco de dados com sucesso!");
         }
+        await using (var connection2 = new NpgsqlConnection(connectionString))
+        {
+            await connection2.OpenAsync();
+            await using (var cmd = connection2.CreateCommand())
+            {
+                cmd.CommandText = "SELECT version()";
+                var version = await cmd.ExecuteScalarAsync();
+                Console.WriteLine($"Versão do PostgreSQL: {version}");
+            }
+        }
     }
-
     /*static void ConnectToDatabase()
     {
         string connectionString = "Host=localhost;Username=postgres;Password=1508;Database=database";
--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20902ab35f0e30a6afc32fdd63f1f26e8c7d08d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8449794c694afcf8a278a24f434b35f145606537")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
