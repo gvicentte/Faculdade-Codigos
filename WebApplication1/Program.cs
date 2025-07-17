20902ab35f0e30a6afc32fdd63f1f26e8c7d08d3
Gabriel Vicentte <gvicentte10@gmail.com>
2025-07-17 08:50:38 -0300

    Add WebApplication1.dll and WebApplication1.refint.dll to the Debug output for .NET 9.0

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -9,5 +9,21 @@ class Program
         Console.Write("Ola Informe seu nome: ");
         nome = Console.ReadLine();
         Console.WriteLine("Ola seu nome e {0}", nome);
+        string connectionString = "Host=localhost;Username=postgres;Password=1508;Database=database";
+        using(var connection=new NpgsqlConnection(connectionString))
+        {
+            connection.Open();
+            Console.WriteLine("Conectado ao banco de dados com sucesso!");
+        }
     }
+
+    /*static void ConnectToDatabase()
+    {
+        string connectionString = "Host=localhost;Username=postgres;Password=1508;Database=database";
+        using(var connection=new NpgsqlConnection(connectionString))
+        {
+            connection.Open();
+            Console.WriteLine("Conectado ao banco de dados com sucesso!");
+        }
+    }*/
 }
--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8db8d8680bff83f8fdb2a4a33447846ecd2f5621")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+450e7f8bae3abc8bf754d757b0b6b0c19e171250")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
